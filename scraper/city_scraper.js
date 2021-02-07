7d356792aef282933902e89a656d0702f1cabb8e
James <jwljacl@gmail.com>
2021-02-07 00:00:56 -0500

    attractions scraping complete

--- scraper/city_scraper.js
+++ scraper/city_scraper.js
@@ -3,6 +3,7 @@ const express = require("express");
 router = express.Router();
 const pool = require("../db/index");
 const axios = require("axios");
+const { text } = require("body-parser");
 
 const cityQuery = (name) => {
   return pool.query("SELECT name, state, TA_code FROM cities WHERE name=$1", [
@@ -10,17 +11,35 @@ const cityQuery = (name) => {
   ]);
 };
 
+// Get top attractions of the city
 router.get("/api/attractions/:cityname", async (req, res) => {
   const { rows } = await cityQuery(req.params.cityname);
   const cityName = rows[0]["name"];
   const stateName = rows[0]["state"];
   const cityCode = rows[0]["ta_code"];
 
+  const fetchList = [];
+
   let url = `https://www.tripadvisor.ca/Attractions-${cityCode}-Activities-${cityName}_${stateName}.html`;
 
   const { data } = await axios.get(url);
   const $ = cheerio.load(data);
-  console.log($("._255i5rcQ > h3").text());
+
+  // obtain attraction item individually
+  var t = $("._255i5rcQ > h3")
+    .contents()
+    .map(function () {
+      return this.type === "text" ? $(this).text() : "";
+    })
+    .get();
+
+  // process data items to remove numbers & undefined data
+  t.map((text) => {
+    if (text.match(/[a-zA-Z]\w+/g)) fetchList.push(text);
+  });
+  const resList = fetchList.slice(0, 10);
+
+  res.json(resList);
 });
 
 module.exports = router;
