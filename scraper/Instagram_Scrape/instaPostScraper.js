3e9a20114022238919af580748d444836a4c35fa
James <jwljacl@gmail.com>
2021-02-16 22:48:41 -0500

    Instagram scraper using Puppeteer & query string

--- scraper/Instagram_Scrape/instaPostScraper.js
+++ scraper/Instagram_Scrape/instaPostScraper.js
@@ -1,22 +1,7 @@
-// TODO: test scraper
+// TODO: test scraper w/o headless: false
 
 const axios = require("axios");
-
-const instaPostScraper = async (addresses) => {
-  let arr = [];
-
-  //const address = addresses["addressList"][0];
-
-  addresses["addressList"].map(async (address) => {
-    const addressFiltered = address.replace(/[^a-zA-Z0-9]/g, "");
-    const url = `https://instagram.com/explore/tags/${addressFiltered}`;
-
-    const response = await axios.get(url);
-    arr.push(instaQlScrape(response));
-  });
-
-  return arr;
-};
+const puppeteer = require("puppeteer");
 
 const instaQlScrape = (response) => {
   let edges = response.data.graphql.hashtag.edge_hashtag_to_top_posts.edges;
@@ -32,6 +17,37 @@ const instaQlScrape = (response) => {
   });
 
   return posts;
+};
+
+const instaPostScraper = async (addresses) => {
+  let arr = [];
+
+  const address = addresses["addressList"][0];
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://www.instagram.com/accounts/login/");
+  await page.waitForSelector('input[name="username"]');
+  await page.type('input[name="username"]', "sensynn@gmail.com");
+  await page.type('input[name="password"]', "KellySnn13678");
+  await page.click('button[type="submit"]');
+  await page.waitForNavigation({ waitUntil: "networkidle0" });
+
+  // addresses["addressList"].map(async (address) => {
+  const addressFiltered = address.replace(/[^a-zA-Z0-9]/g, "");
+  const url = `https://www.instagram.com/explore/tags/${addressFiltered}/?__a=1`;
+
+  await page.goto(url);
+
+  let response = await page.evaluate(() => {
+    return document.querySelector("body").innerText;
+  });
+  //const response = await axios.get(url);
+  console.log(response);
+  //arr.push(instaQlScrape(response));
+  // });
+
+  console.log(arr);
 };
 
 module.exports = instaPostScraper;
