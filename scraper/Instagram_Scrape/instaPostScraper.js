831636237bb02a74847a909ee0a5a27968e631fa
James <jwljacl@gmail.com>
2021-02-17 21:58:34 -0500

    Instagram Scraper complete

--- scraper/Instagram_Scrape/instaPostScraper.js
+++ scraper/Instagram_Scrape/instaPostScraper.js
@@ -3,15 +3,15 @@
 const axios = require("axios");
 const puppeteer = require("puppeteer");
 
-const instaQlScrape = (response) => {
-  let edges = response.data.graphql.hashtag.edge_hashtag_to_top_posts.edges;
+const instaQlScrape = (res) => {
+  let edges = res.graphql.hashtag.edge_hashtag_to_top_posts.edges;
 
   let posts = edges.map((edge) => {
     if (edge)
       return {
-        imgSrc: edge.display_url,
-        timeStamp: edge.taken_at_timestamp,
-        caption: edge.accessibility_caption,
+        imgSrc: edge.node.display_url,
+        timeStamp: new Date(edge.node.taken_at_timestamp), // convert timestamp to readable time
+        caption: edge.node.accessibility_caption,
       };
     else return {};
   });
@@ -26,10 +26,15 @@ const instaPostScraper = async (addresses) => {
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://www.instagram.com/accounts/login/");
+  await page.setUserAgent(
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+  );
+  await page.goto("https://www.instagram.com/accounts/login/", {
+    waitUntil: "networkidle2",
+  });
   await page.waitForSelector('input[name="username"]');
-  await page.type('input[name="username"]', "sensynn@gmail.com");
-  await page.type('input[name="password"]', "KellySnn13678");
+  await page.type('input[name="username"]', "username");
+  await page.type('input[name="password"]', "password");
   await page.click('button[type="submit"]');
   await page.waitForNavigation({ waitUntil: "networkidle0" });
 
@@ -37,15 +42,13 @@ const instaPostScraper = async (addresses) => {
   const addressFiltered = address.replace(/[^a-zA-Z0-9]/g, "");
   const url = `https://www.instagram.com/explore/tags/${addressFiltered}/?__a=1`;
 
-  await page.goto(url);
+  await page.goto(url, { waitUntil: "networkidle2", timeout: 0 });
 
-  let response = await page.evaluate(() => {
-    return document.querySelector("body").innerText;
+  let res = await page.evaluate(() => {
+    return JSON.parse(document.querySelector("body").innerText);
   });
-  //const response = await axios.get(url);
-  console.log(response);
-  //arr.push(instaQlScrape(response));
-  // });
+
+  arr.push(instaQlScrape(res));
 
   console.log(arr);
 };
