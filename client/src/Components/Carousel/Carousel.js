3f2be063fc9df67bcf187f71bd9c28cbc965c9ff
James <jwljacl@gmail.com>
2021-02-28 22:44:18 -0500

    Carousel in progress

--- client/src/Components/Carousel/Carousel.js
+++ client/src/Components/Carousel/Carousel.js
@@ -1,7 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import images from "./images";
+import "./Carousel.css";
 
 const Carousel = () => {
-  return <div></div>;
+  let [sliderNum, setSliderNum] = useState(0);
+
+  const slideReset = (sliderNum) => {
+    if (sliderNum < 0) setSliderNum(Object.keys(images).length - 1);
+    else if (sliderNum > Object.keys(images).length - 1) setSliderNum(0);
+  };
+
+  useEffect(() => {
+    var rotate = setInterval(() => {
+      setSliderNum(sliderNum + 1);
+    }, 7000);
+
+    return () => {
+      clearInterval(rotate);
+    };
+  });
+
+  useEffect(() => {
+    slideReset(sliderNum);
+  }, [sliderNum]);
+
+  return (
+    <div className="Carousel">
+      <div
+        className="Carousel_Pic"
+        style={{
+          backgroundImage: `url(${images[sliderNum]})`,
+        }}
+      ></div>
+      <div
+        className="Carousel_Left"
+        onClick={() => setSliderNum(sliderNum - 1)}
+      >
+        --
+      </div>
+      <div
+        className="Carousel_Right"
+        onClick={() => setSliderNum(sliderNum + 1)}
+      >
+        --
+      </div>
+    </div>
+  );
 };
 
 export default Carousel;
---
+++ client/src/Components/Carousel/images.js
@@ -0,0 +1,17 @@
+import Bangkok from "../../Assets/bangkok.jpg";
+import NYC from "../../Assets/nyc.jpg";
+import London from "../../Assets/london.jpg";
+import Rio from "../../Assets/rio.jpg";
+import Tokyo from "../../Assets/tokyo.jpg";
+import Toronto from "../../Assets/toronto.jpg";
+
+const images = {
+  0: NYC,
+  1: Bangkok,
+  2: London,
+  3: Rio,
+  4: Tokyo,
+  5: Toronto,
+};
+
+export default images;
