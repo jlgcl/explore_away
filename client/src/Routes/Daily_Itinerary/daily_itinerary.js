461b14499cc34b0aaa1f7ccc189159a69a3d3bf0
jlgcl <57466808+jlgcl@users.noreply.github.com>
2021-04-05 23:25:15 -0400

    daily_itinerary in progress: layout complete

--- client/src/Routes/Daily_Itinerary/daily_itinerary.js
+++ client/src/Routes/Daily_Itinerary/daily_itinerary.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./daily_itinerary.css";
 
+import attractionPic from "../../Assets/it_attraction.png";
+import restaurantPic from "../../Assets/it_restaurant.png";
+import hotelPic from "../../Assets/it_hotel.png";
+
 const DailyItinerary = () => {
   const [username, setUsername] = useState(localStorage.getItem("user"));
-  const [fetchData, setFetchData] = useState([{ date: "", itineraries: [""] }]);
+  const [fetchData, setFetchData] = useState(null);
+  const [renderContent, setRenderContent] = useState(null);
 
   const onFetchItinerary = async () => {
     let data = { username: username };
@@ -18,10 +23,39 @@ const DailyItinerary = () => {
     setFetchData(fetchJson);
   };
 
-  /// Itinerary render ///
-  let renderItems = fetchData.map((group) => (
-    <div className="itinerary_item" key={group["date"]}></div>
-  ));
+  const backgroundImgSet = (addressType) => {
+    if (addressType === "attractions")
+      return (
+        <div
+          className="itinerary_address_type_child"
+          style={{
+            backgroundImage: `url(${attractionPic})`,
+            backgroundColor: "rgb(255, 182, 148)",
+          }}
+        ></div>
+      );
+    if (addressType === "restaurants")
+      return (
+        <div
+          className="itinerary_address_type_child"
+          style={{
+            backgroundImage: `url(${restaurantPic})`,
+            backgroundColor: "rgb(192, 185, 255)",
+          }}
+        ></div>
+      );
+    if (addressType === "hotels")
+      return (
+        <div
+          className="itinerary_address_type_child"
+          style={{
+            backgroundImage: `url(${hotelPic})`,
+            backgroundColor: "rgb(164, 255, 187)",
+          }}
+        ></div>
+      );
+    return null;
+  };
 
   useEffect(() => {
     if (username === undefined || username === null) {
@@ -30,7 +64,34 @@ const DailyItinerary = () => {
     } else onFetchItinerary();
   }, []);
 
-  console.log(fetchData);
+  useEffect(() => {
+    if (fetchData !== null) {
+      setRenderContent(
+        fetchData.map((group, index) => (
+          <div className="itinerary_group" key={index}>
+            <div className="itinerary_group_header">{group.date}</div>
+            {group.itineraries.map((itinerary, subindex) => (
+              <div className="itinerary_item" key={subindex}>
+                <div className="itinerary_time">
+                  {itinerary.time.split(", ")[1]}
+                </div>
+                <div
+                  className="itinerary_address_type"
+                  name={itinerary.address_type}
+                >
+                  {backgroundImgSet(itinerary.address_type)}
+                </div>
+                <div className="itinerary_content">
+                  <div className="itinerary_address">{itinerary.address}</div>
+                  <div className="itinerary_city">{itinerary.city}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        ))
+      );
+    }
+  }, [fetchData]);
 
   return (
     <div className="daily_itinerary">
@@ -38,7 +99,7 @@ const DailyItinerary = () => {
         <div className="itinerary_header"></div>
       </div>
       <div className="itinerary_title">{username}'s Daily Itinerary</div>
-      <div className="itinerary_items">{renderItems}</div>
+      <div className="itinerary_items">{renderContent}</div>
     </div>
   );
 };
